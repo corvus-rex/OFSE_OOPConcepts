116b2b8990ef9af7dd0867dd3316095cea28dcbf
Adrianov Corvus <34883039+CorvusHermit@users.noreply.github.com>
2020-07-21 21:01:05 +0700

    Added setter and getter to inheritance

--- inheritance.py
+++ inheritance.py
@@ -1,21 +1,58 @@
 class Person:
-    def __init__(self, fname, lname):
-        self.firstname = fname
-        self.lastname = lname
+    def __init__(self):
+        self._firstname = None
+        self._lastname = None
 
     def printname(self):
-        print(self.firstname, self.lastname)
+        print(self._firstname, self._lastname)
+    
+    @property
+    def firstname(self):
+        print("getter of firstname called")
+        return self._firstname
+
+    @firstname.setter
+    def firstname(self, value):
+        print("setter of firstname called")
+        self._firstname = value
+
+    @property
+    def lastname(self):
+        print("getter of lastname called")
+        return self._lastname
+
+    @lastname.setter
+    def lastname(self, value):
+        print("setter of lastname called")
+        self._lastname = value
 
 class Student(Person):
-    def __init__(self, fname, lname, year):
-        super().__init__(fname, lname)
-        self.graduationyear = year
+    def __init__(self):
+        super().__init__()
+        self._graduationyear = None
+    
+    @property
+    def graduationyear(self):
+        print("getter of graduationyear called")
+        return self._graduationyear
+
+    @graduationyear.setter
+    def graduationyear(self, value):
+        print("setter of graduationyear called")
+        self._graduationyear = value
 
     def printinfo(self):
-        print(self.firstname, self.lastname, self.graduationyear)
+        print(self._firstname, self._lastname, self._graduationyear)
 
-foo = Person("Adrian", "Riadi")
+foo = Person()
+foo.firstname = "Adrian"
+foo.lastname = "Riadi"
+print(foo.firstname)
+print(foo.lastname)
 foo.printname()
 
-bar = Student("Adrian", "Riadi", 2019)
+bar = Student()
+bar.firstname = "Adrian"
+bar.lastname = "Riadi"
+bar.graduationyear = 2019
 bar.printinfo()
