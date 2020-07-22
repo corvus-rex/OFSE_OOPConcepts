61b06333196f6257dd848f6b8a767de65ba76834
Adrianov Corvus <34883039+CorvusHermit@users.noreply.github.com>
2020-07-22 16:22:03 +0700

    Added general polymorphism example

---
+++ polymorphism.py
@@ -0,0 +1,49 @@
+from math import pi
+
+
+class Shape:
+    def __init__(self, name):
+        self.name = name
+
+    def area(self):
+        pass
+
+    def fact(self):
+        return "I am a two-dimensional shape."
+
+    def __str__(self):
+        return self.name
+
+
+class Square(Shape):
+    def __init__(self, length):
+        super().__init__("Square")
+        self.length = length
+
+    def area(self):
+        return self.length**2
+
+    def fact(self):
+        return "Squares have each angle equal to 90 degrees."
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__("Circle")
+        self.radius = radius
+
+    def area(self):
+        return pi*self.radius**2
+
+def print_areas(arr):
+    for obj in arr:
+        print(obj.area())
+
+a = Square(4)
+b = Circle(7)
+print(b)
+print(b.fact())
+print(a.fact())
+
+arr = [a, b]
+print_areas(arr)
